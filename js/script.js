c6610c074a3f3fa74b880b19ad2dce5449d9a162
ahnafhemal <hemalhasan140@gmail.com>
2022-12-04 11:25:20 +0600

    five

--- js/script.js
+++ js/script.js
@@ -11,7 +11,29 @@ function updateProductNumber(casefield, allow, casetotal, price) {
     //update case total
     const productTotal = document.getElementById(casetotal);
     productTotal.innerText = productNumber * price;
+    //calculate Total
+    calculateTotal()
 }
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number-field');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax
+    //update on the html
+    document.getElementById('sub-total').innerText = subTotal
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('final-total').innerText = totalPrice;
+
+}
+
 
 document.getElementById('btn-case-plus').addEventListener('click', function () {
     updateProductNumber('case-number-field', true, 'case-total', 59)
@@ -29,3 +51,15 @@ document.getElementById('btn-phone-plus').addEventListener('click', function () 
 document.getElementById('btn-phone-minus').addEventListener('click', function () {
     updateProductNumber('phone-number-field', false, 'phone-total', 1219)
 })
+
+
+
+
+
+
+
+
+
+//sub-total
+//tax-amount
+//final-total
